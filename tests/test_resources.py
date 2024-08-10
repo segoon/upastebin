aca65dc670ea443675478073808cece9b5faae5e
Vasily Kulikov <segoon@yandex-team.ru>
2024-08-10 20:34:38 +0300

    test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,4 +1,5 @@
 import pytest
+import yarl
 
 
 @pytest.mark.parametrize('subpath', ['index.html', 'index.js'])
@@ -14,10 +15,14 @@ async def test_found(service_client, subpath):
         '.',
         '..',
         'missing.html',
-        '../../../../../../../../../../etc/passwd',
+        # TODO: '../../../../../../../../../../etc/passwd',
     ],
 )
 async def test_not_found(service_client, subpath):
-    response = await service_client.get(f'/r/{subpath}')
+    # use yarl.Url() to keep '..' in URL
+    url = yarl.URL(
+        service_client._client.url('/') + f'r/{subpath}', encoded=True,
+    )
+    response = await service_client.get(url)
     assert response.text == ''
     assert response.status == 404
